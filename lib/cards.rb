cb3cf00a32f206e39b8c074b47f1ad810385a46b
aprietof <adrianprieto00@gmail.com>
2016-03-14 21:03:08 -0400

    first commit

---
+++ lib/cards.rb
@@ -0,0 +1,5 @@
+require "cards/version"
+
+module Cards
+  # Your code goes here...
+end
---
+++ spec/cards_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Cards do
+  it 'has a version number' do
+    expect(Cards::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
